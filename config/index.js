026beaba5c6ff7aee8e38a6ea8d97f70bb08d0a4
guonanci <yiminanci@gmail.com>
2018-11-14 09:49:45 +0800

    express

--- config/index.js
+++ config/index.js
@@ -1,2 +1,29 @@
 const path = require('path')
 
+module.exports = {
+  dev: {
+    mode: 'development',
+    autoOpenBrowser: true,
+    errorOverlay: true,
+    notifyOnErrors: true,
+    useEslint: false,
+    showEslintErrorsInOverlay: false,
+    poll: false,
+    host: 'localhost',
+    port: 7777,
+    assetsSubDir: 'static',
+    assetsPublicPath: '/',
+    proxyTable: {
+      '/api': {
+        'target': 'https://qz-uat.xinyan.com',
+        'changeOrigin': true,
+        'pathRewrite': { '^/api': '/api' }
+      }
+    },
+    context: [
+      '/api',
+    ]
+    proxypath: '',
+    cssSourceMap: false,
+  }
+}
